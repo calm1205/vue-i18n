f97f8211bc292adc59969f1ff7c328503087e927
calm1205 <nagi.moriyama@mntsq.com>
2025-07-28 07:45:22 +0900

    feat: component i18n

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import { useI18n } from "vue-i18n"
+
+import HelloWorld from "./components/HelloWorld.vue"
+
+const { t } = useI18n()
 </script>
 
 <template>
-  <HelloWorld msg="vue-i18n" />
+  <p>{{ t("hello") }}</p>
+  <HelloWorld />
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
-import { createApp } from "vue";
+import { createApp } from "vue"
+import { createI18n } from "vue-i18n"
 
-import "./style.css";
-import App from "./App.vue";
+import "./style.css"
+import App from "./App.vue"
 
-createApp(App).mount("#app");
+const locale = import.meta.env.MODE === "en" ? "en" : "ja"
+const i18n = createI18n({
+  locale,
+  legacy: false,
+  fallbackLocale: "ja",
+  messages: {
+    ja: {
+      hello: "こんにちは",
+    },
+    en: {
+      hello: "Hello",
+    },
+  },
+})
+
+createApp(App).use(i18n).mount("#app")
+
+// 後から代入でlocaleを変更
+// i18n.global.locale.value = "ja"
