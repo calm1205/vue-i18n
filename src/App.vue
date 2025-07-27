2627aa3991839e1224ddfd8fcfd294d3d81ead42
calm1205 <nagi.moriyama@mntsq.com>
2025-07-28 07:29:41 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,7 @@ import HelloWorld from "./components/HelloWorld.vue"
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="vue-i18n" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
